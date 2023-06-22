fac2a07ee89ab20dc650f2a8cbbef15a4594bf13
Luis Martinez <rickymtz1999@gmail.com>
2023-06-22 17:34:06 -0500

    adds avatar to the table

--- sql/users.sql
+++ sql/users.sql
@@ -6,9 +6,14 @@ CREATE TABLE users (
     id INT PRIMARY KEY,
     email TEXT,
     first_name TEXT,
+    last_name TEXT,
     username VARCHAR(50),
+    avatar URL, 
     password TEXT
 );
 
-INSERT INTO users (id, email, first_name, last_name, username, password)
+INSERT INTO users (id, email, first_name, last_name, username, avatar, password)
 VALUES (1, 'henry@gmail.com', 'Henry', 'Martinez', '1234') 
+
+
+SELECT * FROM users;
