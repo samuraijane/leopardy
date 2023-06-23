18d37fd2f7022084022cf1b3e2a793dfbba4f353
Jayy2023 <119895456+Jayy2023@users.noreply.github.com>
2023-06-22 21:52:03 -0400

    reinstalled dep

--- react-ui/src/app.tsx
+++ react-ui/src/app.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { Footer, Header, Main } from '../sectioning';
 
@@ -9,13 +8,6 @@ const App = (): JSX.Element => {
       <Header />
       <Main />
       <Footer />
-=======
-import SignUp from "./components/sign-up";
-const App = (): JSX.Element => {
-  return (
-    <>
-    <SignUp />
->>>>>>> a40e2a7 (create sign up component)
     </>
   );
 };
--- react-ui/src/index.tsx
+++ react-ui/src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
-<<<<<<< HEAD
 import { BrowserRouter } from 'react-router-dom'
 
 import "./styles/style.scss";
@@ -13,17 +12,6 @@ ReactDOM.render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
-=======
-import "./styles/style.scss";
-import SignUp from "./components/sign-up";
-
-const container: HTMLElement | null = document.getElementById("root");
-
-
-ReactDOM.render(
-  <React.StrictMode>
-    <SignUp />
->>>>>>> a40e2a7 (create sign up component)
   </React.StrictMode>,
   container
 );
