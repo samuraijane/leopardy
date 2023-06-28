d0a78a885aa9b9194557f5e1891f3b327dea8772
BuildBelleza <123292150+BuildBelleza@users.noreply.github.com>
2023-06-27 20:36:15 -0400

    add footer code and style

--- react-ui/src/sectioning/footer.jsx
+++ react-ui/src/sectioning/footer.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer > 
-      <div className='y-wrap'>Footer</div>
+      <div className='y-wrap'>
+        <ul className='footer'>
+          <li className='footer'> <a> Privacy Policy</a></li>
+          <li className='footer'> <a> Copyright 2023</a></li>
+          <li className='footer'> <a href='https://github.com/'> GitHub </a></li>
+          <li className='footer'> <a href='/about'> About </a></li>
+      </ul>
+      
+        
+        </div>
     </footer>
   )
 };
