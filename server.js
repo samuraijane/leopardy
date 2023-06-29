cf66e07ea91dbcf36667e9db15e210aafc0c1769
Dezarea Bryan <dezareabryan@gmail.com>
2023-06-29 01:46:38 -0400

    connected database to server and created basic route to show all users from the database.

--- server.js
+++ server.js
@@ -5,15 +5,37 @@ const sessions = require('express-session');
 const pgp = require('pg-promise')();
 const cors = require('cors');
 const bcrypt = require('bcrypt');
-const express = require('express')
+const express = require('express');
 
 const server = express();
 
-const PORT = 8080
+const PORT = 8080;
+
+const db = pgp({
+  host: 'localhost',
+  port: 5432,
+  database: 'leopardy',
+  user: 'postgres',
+  password: process.env.PASSWORD, 
+});
+
+server.use(cors());
+server.use(express.json());
+server.use(cookieParser());
 
 server.get('/heartbeat', (req, res) => {
-    res.json({message:"heartbeat"})
-} )
+  res.json({ message: 'heartbeat' });
+});
+
+server.get('/users', async (req, res) => {
+  try {
+    const users = await db.manyOrNone('SELECT * FROM users');
+    res.json(users);
+  } catch (error) {
+    console.error('Error retrieving users:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 
-server.listen(PORT, () => console.log (`this server is running at PORT ${PORT}`));
+server.listen(PORT, () => console.log(`This server is running at PORT ${PORT}`));
