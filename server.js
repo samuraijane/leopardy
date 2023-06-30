bc4f18d9dd3905e796b4ecbd6560ab9e9a8ecea8
Maggie <zach.benton.27@gmail.com>
2023-06-29 21:12:55 -0400

    add avatar endpoint

--- server.js
+++ server.js
@@ -15,5 +15,10 @@ server.get('/heartbeat', (req, res) => {
     res.json({message:"heartbeat"})
 } )
 
+server.get('api/avatars', async (req, res) => {
+    const userInput = req.body;
+    const img = await fetch(`https://robohash.org/${userInput}?set=set3`);
+    res.json(img);
+})
 
 server.listen(PORT, () => console.log (`this server is running at PORT ${PORT}`));
