1923484902b34c7589277f880efca3f6ef4c320d
Jordan Couch <119895456+Jayy2023@users.noreply.github.com>
2023-06-29 20:41:38 -0400

    002/app dep vercel (#57)
    * moved and updated verceljson file
    
    * adding double quotes to reactrouterdom

--- react-ui/src/index.tsx
+++ react-ui/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from "react-router-dom";
 
 import "./styles/style.scss";
 
