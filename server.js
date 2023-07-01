5fc997f0e2709eef2c5384d4bdd44ff6b24b1fcd
Maggiejackss <121919568+Maggiejackss@users.noreply.github.com>
2023-07-01 10:26:12 -0400

    add avatar endpoint (#61)

--- server.js
+++ server.js
@@ -27,6 +27,12 @@ server.get('/heartbeat', (req, res) => {
   res.json({ message: 'heartbeat' })
 })
 
+server.get('api/avatars', async (req, res) => {
+    const userInput = req.body;
+    const img = await fetch(`https://robohash.org/${userInput}?set=set3`);
+    res.json(img);
+})
+
 server.get('/users', async (req, res) => {
   try {
     const users = await db.manyOrNone('SELECT * FROM users')
