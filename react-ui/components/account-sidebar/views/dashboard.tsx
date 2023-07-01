eab90caf9968f86bb23afd8190306c1fa4c5d2bd
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-07-01 13:01:16 -0500

    add gmail to the dashboard

--- react-ui/components/account-sidebar/views/dashboard.tsx
+++ react-ui/components/account-sidebar/views/dashboard.tsx
@@ -16,12 +16,21 @@ interface UserDataProps {
     userData: DashboardProps
 }
 
+interface EmailProp {
+    email: string
+}
+
 const Dashboard: React.FC<UserDataProps> = ({ userData }): JSX.Element => {
   return (
     <Container>
       <Wrapper>
-        <WelcomeText />
-        <UsernameText user={userData.username} />
+        <ColumnContainer>
+            <WelcomeText />
+            <UsernameText user={userData.username} />
+        </ColumnContainer>
+        <ColumnContainer>
+            <EmailText email={userData.email}/>
+        </ColumnContainer>
       </Wrapper>
     </Container>
   )
@@ -33,29 +42,50 @@ const Container = styled.div`
   height: 100%;
   padding: 12px;
   border-left: 4px;
+  background: #f1f1f1;
+  border-bottom-right-radius: 8px;
+  border-top-right-radius: 8px;
 `
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: row;
-  gap: 4px;
-  font-size: 16px;
-  text-transform: uppercase;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 20px;
+  text-transform: capitalize;
   color: #000;
+  padding: 14px;
 `
 
 const Welcome = styled.h1`
-
+  color: #1e7faf;
 `
 const WelcomeText = () => {
   return <Welcome>Welcome</Welcome>
 }
 
 const Username = styled.span`
-  font-weight: bold;
-  font-size: 20px;
+  font-size: 18px;
+  text-transform: lowercase;
+  margin-left: 6px;
 `
 
 const UsernameText = ({ user }: UsernameProp) => {
   return <Username>{user}</Username>
+}
+
+const ColumnContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+`
+
+const Email = styled.span`
+    text-transform: lowercase;
+    font-size: 18px;
+
+`
+const EmailText = ({email}: EmailProp) => {
+    return <Email>{email}</Email>
 }
