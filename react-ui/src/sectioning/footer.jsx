930ba5ee22eb3bb3b7840b65e960adb1dea77fe0
BuildBelleza <123292150+BuildBelleza@users.noreply.github.com>
2023-10-04 19:13:54 -0400

    add time to footer (#70)

--- react-ui/src/sectioning/footer.jsx
+++ react-ui/src/sectioning/footer.jsx
@@ -3,14 +3,23 @@ import styled from 'styled-components';
 
   
 const Footer = () => {
+  const getTime = () => {
+    const date = new Date();
+    const currentTime = date.toLocaleTimeString('en-US', { hour12: false, hour: 'numeric', minute: 'numeric', second: 'numeric' });
+    return currentTime;
+  }
+
+  
+  
   return (
     <Container  > 
       <div  className='y-wrap'>
         <ul className='footer'>
-          <li className='footer'> <a> Privacy Policy</a></li>
-          <li className='footer'> <a> Copyright 2023</a></li>
+          <li className='footer'> <a>Privacy Policy</a></li>
+          <li className='footer'> <a>Copyright 2023</a></li>
           <li className='footer'> <a href='https://github.com/'> GitHub </a></li>
           <li className='footer'> <a href='/about-us'> About </a></li>
+          <li className='footer'> <a>{getTime()}</a></li>
       </ul>
       
         
@@ -18,6 +27,8 @@ const Footer = () => {
     </Container>
   )
 };
+
+
 
 export default Footer;
 
@@ -43,28 +54,28 @@ const Container = styled.footer`
     background-color: dark grey;
   }
   .footer li {
-    width: 25%;
+    width: 20%;
     color: white;
     
   }
   .footer ul {
-    width: 25%;
+    width: 20%;
     color: white;
    
   }
   .footer ul li {
-    width: 25%;
+    width: 20%;
     color: white;
     
   }
   .footer ul li a {
-    width: 25%;
+    width: 20%;
     color: white;
     height: 20px;
     
   }
   .footer ul li a:hover {
-    width: 25%;
+    width: 20%;
     color: black;
     height: 20px;
   }
@@ -76,4 +87,5 @@ const Container = styled.footer`
     width-max: 20px;
   }
 
+  
 `;
